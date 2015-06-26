a8eb35f33e449cc9e2daeaea1039e40df4e55674
Ed <ed@panhandlesoftware.com>
2015-06-26 10:44:33 -0700

    added todo comments

--- requestAgent/injest.php
+++ requestAgent/injest.php
@@ -1,5 +1,6 @@
 <?php
 
+// TODO: this error handler doesn't seem to be working need to do more research
 function myErrorHandler($errno, $errstr, $errfile, $errline)
 {
         echo " [$errno] $errstr \n";
@@ -14,9 +15,11 @@ echo " starting injest process... \n";
 //require_once__DIR__.'/vendor/autoload.php';
 require_once('/home/ed/requestAgent/vendor/autoload.php');
 
+// load the Rabbit client classes
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+// retrieve the request data and parse to json
 $json = file_get_contents('php://input');
 echo " json string \n";
 var_dump($json);
@@ -26,8 +29,7 @@ var_dump($obj);
 echo " REQUEST object \n";
 var_dump($_REQUEST);
 
-$bytes = 150;
-
+// connect to rabbitMQ
 echo " opening connection... \n";
 $connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
 //if($connection->isConnected() == false) { echo " no connection established\n"; };
@@ -36,15 +38,22 @@ echo " getting channel... \n";
 $ch = $connection->channel();
 if($ch->is_open !== true) { echo " no channel object"; };
 
+// create the exchange and bind to the attributes queue
+// TODO: need to have the exchange name come from rabbit
 $ch->exchange_declare('attributeExchange', $type = "direct", false, false, $auto_delete = true);
 $ch->queue_bind('attributes', 'attributeExchange', 'attribute');
 
+// create a new message that we will reuse for all the values in the request
 echo " setting up new message... \n";
 $body = "place holder";
 $properties = array('content_type' => 'text/plain', 'delivery_mode' => 2);
 $msg = new AMQPMessage($body, $properties);
+
+// todo: would like to set data limit here, more research needed
+//$bytes = 150;
 //$msg->setBodySizeLimit($bytes);
 
+// loop through each json object and publish the value to rabbit
 foreach($obj['data'] as $array) {
 
         $body = '{"key":"' . $array['key'] . '", "value":"' . $array['value'] . '"}';
@@ -53,6 +62,7 @@ foreach($obj['data'] as $array) {
         $ch->basic_publish($msg, 'attributeExchange', 'attribute');
 }
 
+// todo: need to find out why connection closing was failing, and try to modularize the connection, possibly pooling?
 //echo " closing channel and connection.\n";
 //$ch->close();
 //$connection->close();
